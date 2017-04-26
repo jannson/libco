f9f11a9bc8720ba3e6b9cb74883e4b65d580438a
janson <gandancing@gmail.com>
2017-04-26 12:23:17 +0800

    co switch ok

--- co_test1.c
+++ co_test1.c
@@ -3,6 +3,9 @@
 #include "settings.h"
 #include "libco.h"
 
+cothread_t lth1 = NULL;
+cothread_t lth2 = NULL;
+
 //arm64: https://github.com/fluent/fluent-bit/issues/228
 static int add(int a, int b, int c) {
     printf("test a=%d b=%d c=%d\n", a,b,c);
@@ -18,15 +21,14 @@ static void testco2() {
 static void testco() {
     printf("in testco\n");
     printf("result=%d\n", add(1,2,3));
-    size_t out_size = 0;
-    cothread_t lth = co_create(4096, testco2, &out_size);
-    co_switch(lth);
+    co_switch(lth2);
 }
 
 int main() {
     size_t out_size = 0;
-    cothread_t lth = co_create(4096, testco, &out_size);
-    co_switch(lth);
+    lth1 = co_create(4096, testco, &out_size);
+    lth2 = co_create(4096, testco2, &out_size);
+    co_switch(lth1);
     return 0;
 }
 
